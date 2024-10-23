8b8893f146fe08cd0a68915947cf4d75a0ffe874
qi1n <cinnshaoqing@gmail.com>
2024-10-23 18:44:10 +0200

    global database link variable

--- web/src/global.js
+++ web/src/global.js
@@ -1,9 +1,11 @@
 const GRADIO_LOCAL_LINK = "http://0.0.0.0:7862"
 const GRADIO_GLOBAL_LINK = "http://0.0.0.0:7862"
+const DATABASE_LINK = "http://0.0.0.0:3307"
 const LLM_SERVICE_TPYE = "Llama"
 
 export default {
     GRADIO_LOCAL_LINK,
     GRADIO_GLOBAL_LINK,
+    DATABASE_LINK,
     LLM_SERVICE_TPYE
 }
