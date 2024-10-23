787199633685aa15e78a700702d4a0487ea1577d
qi1n <cinnshaoqing@gmail.com>
2024-10-23 16:55:03 +0200

    Reapply "Merge branch 'main' of https://github.com/Kleinpenny/VCTEVA"
    This reverts commit 54a852d2340124edec3c40954196aee7809825cb.

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import gradio as gr
-from llm.chatbot import ChatBot
-from llm.hf_llm_client import HuggingFaceLLMClient
-from llm.aws_bedrock_client import AWSBedrockLLMClient
-
+from Chatbot.Master_Agent import master_main
 
 class GradioInterface:
-    def __init__(self, chatbot: ChatBot):
-        self.chatbot = chatbot
+    def __init__(self):
         self.demo = gr.ChatInterface(
-            self.chatbot.respond
+            master_main
         ).queue()
 
     def launch(self):
@@ -16,20 +12,7 @@ class GradioInterface:
 
 
 def main():
-    # 使用HuggingFace模型
-    # llm_client = HuggingFaceLLMClient(
-    #     "meta-llama/Meta-Llama-3-8B-Instruct",
-    #     token="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"
-    # )
-
-    # 使用AWS Bedrock模型
-    llm_client = AWSBedrockLLMClient(
-        model_id="amazon.titan-text-express-v1",
-        region_name="eu-central-1"  # 请根据您的AWS配置更改区域
-    )
-
-    chatbot = ChatBot(llm_client)
-    interface = GradioInterface(chatbot)
+    interface = GradioInterface()
     interface.launch()
 
 
